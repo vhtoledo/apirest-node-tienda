831424acf0423eef8bce9207e3656ded7351f9fb
victor toledo <querynetworking@gmail.com>
2023-05-27 00:03:58 -0300

    creacion de servidor node

---
+++ index.js
@@ -0,0 +1,25 @@
+const { conexion } = require("./database/conexion");
+const express = require("express");
+const cors = require("cors");
+
+console.log("holaaaaa")
+
+// Conectar a la base de datos
+conexion();
+
+// Crear servidor Node
+const app = express();
+const puerto = 3900;
+
+// Configuracion cors
+app.use(cors());
+
+// Convertir body a objeto JS
+app.use(express.json());
+
+// Crear Rutas 
+
+// Crear servidor y escuchar peticiones http
+app.listen(puerto, () => {
+    console.log("Servidor corriendo en el puerto "+puerto);
+});
