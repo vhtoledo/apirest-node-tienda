25540b5980ffab85f4bf95f6af356b086d18644f
victor toledo <querynetworking@gmail.com>
2023-05-26 23:59:01 -0300

    creacion de conexion a base de datos

---
+++ database/conexion.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const conexion = async () => {
+
+    try {
+        await mongoose.connect("mongodb://localhost:27017/mi_blog");
+
+        console.log("Conectado a la base de datos")
+    } catch (error) {
+        console.log(error);
+        throw new Error("No se a podido conectar a la base de datos");
+    }
+}
+
+module.exports = {
+    conexion
+}
