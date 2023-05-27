b22c71b639285bed1e80fd35b8048d9607c51f27
victor toledo <querynetworking@gmail.com>
2023-05-27 20:18:21 -0300

    creacion de helper validation

--- controllers/articulo.js
+++ controllers/articulo.js
@@ -1,4 +1,4 @@
-const validator = require("validator");
+const {validarArticulo} = require("../helper/validar");
 const Articulo = require("../models/Articulo");
 
 // Metodo de registro
@@ -8,14 +8,9 @@ const crear = (req, res) => {
 
   // Validar Datos
   try {
-    let validar_titulo =
-      !validator.isEmpty(parametros.titulo) &&
-      validator.isLength(parametros.titulo, { min: 5, max: undefined });
-    let validar_contenido = !validator.isEmpty(parametros.contenido);
 
-    if (!validar_titulo || !validar_contenido) {
-      throw new Error("No se ha validado la información");
-    }
+    validarArticulo(parametros);
+
   } catch (error) {
     return res.status(400).json({
       status: "error",
@@ -147,14 +142,9 @@ const editar = (req, res) => {
 
   // Validar datos
   try {
-    let validar_titulo =
-      !validator.isEmpty(parametros.titulo) &&
-      validator.isLength(parametros.titulo, { min: 5, max: undefined });
-    let validar_contenido = !validator.isEmpty(parametros.contenido);
 
-    if (!validar_titulo || !validar_contenido) {
-      throw new Error("No se ha validado la información");
-    }
+    validarArticulo(parametros);
+
   } catch (error) {
     return res.status(400).json({
       status: "error",
