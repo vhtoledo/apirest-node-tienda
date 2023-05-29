e1839fff33d355c556957c705d5dc6e8a3fe8d03
Victor Hugo Toledo <121471152+vhtoledo@users.noreply.github.com>
2023-05-28 23:50:06 -0300

    sistema tienda

--- database/conexion.js
+++ database/conexion.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const conexion = async () => {
 
     try {
-        await mongoose.connect("mongodb://localhost:27017/mi_blog");
+        await mongoose.connect("");
 
         console.log("Conectado a la base de datos")
     } catch (error) {
